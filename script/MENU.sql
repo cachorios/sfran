e9d30e3385b0b9d4b65c70dd016b24aeed7982f3
DarioLopez1856 <dariolopez1856@gmail.com>
2018-10-24 11:31:09 -0300

    Se completa el menu

--- script/MENU.sql
+++ script/MENU.sql
@@ -19,9 +19,13 @@
            (6,  NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PERS', 'Comisionistas',                  5, 2, FALSE, NULL,  2,  5,  'com.gmail.sanfrancisco.view.comisionista.ComisionistaView', 1),
            (7,  NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PERS', 'Consignatarios',                 6, 2, FALSE, NULL,  3,  5,  'com.gmail.sanfrancisco.view.consignatario.ConsignatarioView', 1),
            (8,  NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PERS', 'Productores',                    7, 2, FALSE, NULL,  4,  5,  'com.gmail.sanfrancisco.view.productor.ProductorView', 1),
+           (9,  NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PERS', 'Renspa',                         8, 2, FALSE, NULL,  5,  5,  'com.gmail.sanfrancisco.view.renspa.RenspaView', 1),
 
-           (9,  NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PREF', 'Prefaena',                       8, 2, FALSE, NULL,  2,  0,  'Prefaena', 1),
-           (10, NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PREF', 'DTE',                            9, 2, FALSE, NULL,  3,  9,  'com.gmail.sanfrancisco.view.dte.DteView', 1),
+           (10, NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PREF', 'Prefaena',                       9, 2, FALSE, NULL,  2,  0,  'Prefaena', 1),
+           (11, NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PREF', 'DTE',                           10, 2, FALSE, NULL,  3, 10,  'com.gmail.sanfrancisco.view.dte.DteView', 1),
+
+           (12, NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.OTRO', 'Otros',                         11, 2, FALSE, NULL,  3,  0,  'Otros', 1),
+           (13, NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.OTRO', 'Insumos',                       12, 2, FALSE, NULL,  4, 12,  'com.gmail.sanfrancisco.view.insumo.InsumoView', 1),
 
 
            (90, NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PARA', 'Parametros del Sistema',       900, 2, FALSE, NULL,  8,   0,  'Configuracion General del Sistema', 1),
