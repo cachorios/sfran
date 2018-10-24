36f9e5c2eeaf74dafe9686f42e8b60303d321b54
DarioLopez1856 <dariolopez1856@gmail.com>
2018-10-24 11:50:44 -0300

    Se quita licencia y renspa del menu

--- script/MENU.sql
+++ script/MENU.sql
@@ -13,19 +13,17 @@
 
            (2,  NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.TRAN', 'Vehiculos',                      1, 2, FALSE, NULL,  1,  1,  'com.gmail.sanfrancisco.view.vehiculo.VehiculoView', 1),
            (3,  NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.TRAN', 'Conductores',                    2, 2, FALSE, NULL,  2,  1,  'com.gmail.sanfrancisco.view.conductor.ConductorView', 1),
-           (4,  NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.TRAN', 'Licencias',                      3, 2, FALSE, NULL,  3,  1,  'com.gmail.sanfrancisco.view.licencia.LicenciaView', 1),
 
-           (5,  NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PERS', 'Personas',                       4, 2, FALSE, NULL,  1,  0,  'Personas', 1),
-           (6,  NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PERS', 'Comisionistas',                  5, 2, FALSE, NULL,  2,  5,  'com.gmail.sanfrancisco.view.comisionista.ComisionistaView', 1),
-           (7,  NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PERS', 'Consignatarios',                 6, 2, FALSE, NULL,  3,  5,  'com.gmail.sanfrancisco.view.consignatario.ConsignatarioView', 1),
-           (8,  NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PERS', 'Productores',                    7, 2, FALSE, NULL,  4,  5,  'com.gmail.sanfrancisco.view.productor.ProductorView', 1),
-           (9,  NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PERS', 'Renspa',                         8, 2, FALSE, NULL,  5,  5,  'com.gmail.sanfrancisco.view.renspa.RenspaView', 1),
+           (4,  NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PERS', 'Personas',                       4, 2, FALSE, NULL,  1,  0,  'Personas', 1),
+           (5,  NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PERS', 'Comisionistas',                  5, 2, FALSE, NULL,  2,  4,  'com.gmail.sanfrancisco.view.comisionista.ComisionistaView', 1),
+           (6,  NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PERS', 'Consignatarios',                 6, 2, FALSE, NULL,  3,  4,  'com.gmail.sanfrancisco.view.consignatario.ConsignatarioView', 1),
+           (7,  NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PERS', 'Productores',                    7, 2, FALSE, NULL,  4,  4,  'com.gmail.sanfrancisco.view.productor.ProductorView', 1),
 
-           (10, NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PREF', 'Prefaena',                       9, 2, FALSE, NULL,  2,  0,  'Prefaena', 1),
-           (11, NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PREF', 'DTE',                           10, 2, FALSE, NULL,  3, 10,  'com.gmail.sanfrancisco.view.dte.DteView', 1),
+           (8,  NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PREF', 'Prefaena',                       8, 2, FALSE, NULL,  2,  0,  'Prefaena', 1),
+           (9,  NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PREF', 'DTE',                            9, 2, FALSE, NULL,  3,  8,  'com.gmail.sanfrancisco.view.dte.DteView', 1),
 
-           (12, NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.OTRO', 'Otros',                         11, 2, FALSE, NULL,  3,  0,  'Otros', 1),
-           (13, NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.OTRO', 'Insumos',                       12, 2, FALSE, NULL,  4, 12,  'com.gmail.sanfrancisco.view.insumo.InsumoView', 1),
+           (10, NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.OTRO', 'Otros',                         10, 2, FALSE, NULL,  3,  0,  'Otros', 1),
+           (11, NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.OTRO', 'Insumos',                       11, 2, FALSE, NULL,  4, 10,  'com.gmail.sanfrancisco.view.insumo.InsumoView', 1),
 
 
            (90, NULL, '2017-11-03', 'UNKNOW', 'UNKNOW', 'MENU.PARA', 'Parametros del Sistema',       900, 2, FALSE, NULL,  8,   0,  'Configuracion General del Sistema', 1),
