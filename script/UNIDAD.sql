0d95a494c7a04e3777798661910e5bfed12ca501
DarioLopez1856 <dariolopez1856@gmail.com>
2018-10-31 10:26:47 -0300

    Se arregla el 'nombre' de los parametros UNIDAD

--- script/UNIDAD.sql
+++ script/UNIDAD.sql
@@ -1,4 +1,4 @@
-INSERT INTO parametro (id, usuarioalta, usuarioumod, clase, nombre, orden, tipo, version) VALUES ((36301), 'UNKNOW', 'UNKNOW', 'COLOR', 'UNIDAD', 1, 23, 1);
-INSERT INTO parametro (id, usuarioalta, usuarioumod, clase, nombre, orden, tipo, version) VALUES ((36302), 'UNKNOW', 'UNKNOW', 'COLOR', 'KILOS', 2, 23, 1);
-INSERT INTO parametro (id, usuarioalta, usuarioumod, clase, nombre, orden, tipo, version) VALUES ((36303), 'UNKNOW', 'UNKNOW', 'COLOR', 'METROS', 3, 23, 1);
-INSERT INTO parametro (id, usuarioalta, usuarioumod, clase, nombre, orden, tipo, version) VALUES ((36304), 'UNKNOW', 'UNKNOW', 'COLOR', 'LITROS', 4, 23, 1);
+INSERT INTO parametro (id, usuarioalta, usuarioumod, clase, nombre, orden, tipo, version) VALUES ((36301), 'UNKNOW', 'UNKNOW', 'UNIDAD', 'UNIDAD', 1, 23, 1);
+INSERT INTO parametro (id, usuarioalta, usuarioumod, clase, nombre, orden, tipo, version) VALUES ((36302), 'UNKNOW', 'UNKNOW', 'UNIDAD', 'KILOS', 2, 23, 1);
+INSERT INTO parametro (id, usuarioalta, usuarioumod, clase, nombre, orden, tipo, version) VALUES ((36303), 'UNKNOW', 'UNKNOW', 'UNIDAD', 'METROS', 3, 23, 1);
+INSERT INTO parametro (id, usuarioalta, usuarioumod, clase, nombre, orden, tipo, version) VALUES ((36304), 'UNKNOW', 'UNKNOW', 'UNIDAD', 'LITROS', 4, 23, 1);
