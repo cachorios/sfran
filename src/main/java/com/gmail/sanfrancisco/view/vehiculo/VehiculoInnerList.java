322021bce8f7d8987cbf0732590e409b3cf59382
DarioLopez1856 <dariolopez1856@gmail.com>
2018-11-17 09:07:04 -0300

    Se comprueba los estados de los registros en las consultas de los reportes (fechabaja)

--- src/main/java/com/gmail/sanfrancisco/view/vehiculo/VehiculoInnerList.java
+++ src/main/java/com/gmail/sanfrancisco/view/vehiculo/VehiculoInnerList.java
@@ -12,6 +12,7 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.server.StreamResource;
+import com.vaadin.flow.server.VaadinServlet;
 import org.vaadin.alejandro.PdfBrowserViewer;
 
 import java.util.Date;
@@ -42,9 +43,11 @@ public class VehiculoInnerList extends DefaultInnerListPolymer<Vehiculo> {
 
     private Map<String, Object> crearParametroReporte() {
         filtro = ((IPresenterList) (this.getPresentable().getPresenter())).getDataProvider().getFiltro();
+        String directorio = VaadinServlet.getCurrent().getServletContext().getRealPath("/frontend/images");
 
         Map<String, Object> mapa = new HashMap<String, Object>();
         mapa.put(C.SYS_REP_PARAM_ID, filtro);
+        mapa.put(C.SYS_REP_PARAM_DIRECTORIO, directorio);
         return mapa;
     }
 
