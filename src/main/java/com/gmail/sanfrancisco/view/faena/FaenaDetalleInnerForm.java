d104563fcad5a2801ba588e0307389b1235fa221
DarioLopez1856 <dariolopez1856@gmail.com>
2018-12-11 19:49:13 -0300

    Componente de detalle faena

---
+++ src/main/java/com/gmail/sanfrancisco/view/faena/FaenaDetalleInnerForm.java
@@ -0,0 +1,16 @@
+package com.gmail.sanfrancisco.view.faena;
+
+import com.gmail.cacho.slapi.view.customs.params.ParamCSComponent;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.textfield.TextField;
+
+public class FaenaDetalleInnerForm extends Div {
+    private ParamCSComponent categoria;
+    private TextField cantidad;
+    private TextField faenado;
+    private TextField diferencia;
+    private TextField kgVivo;
+    private TextField aFaenar;
+
+
+}
