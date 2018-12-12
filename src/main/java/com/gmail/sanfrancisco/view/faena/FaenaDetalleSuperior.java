b2e962c2df30cdf70aca9d963e45788e6de93452
DarioLopez1856 <dariolopez1856@gmail.com>
2018-12-11 23:58:38 -0300

    Componentes de detalles de faena

--- src/main/java/com/gmail/sanfrancisco/view/faena/FaenaDetalleSuperior.java
+++ src/main/java/com/gmail/sanfrancisco/view/faena/FaenaDetalleSuperior.java
@@ -4,13 +4,22 @@ import com.gmail.cacho.slapi.view.customs.params.ParamCSComponent;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.textfield.TextField;
 
-public class FaenaDetalleInnerForm extends Div {
+public class FaenaDetalleSuperior extends Div {
     private ParamCSComponent categoria;
     private TextField cantidad;
     private TextField faenado;
     private TextField diferencia;
     private TextField kgVivo;
     private TextField aFaenar;
+
+    public FaenaDetalleSuperior(){
+        add(categoria);
+        add(cantidad);
+        add(faenado);
+        add(diferencia);
+        add(kgVivo);
+        add(aFaenar);
+    }
 
 
 }
