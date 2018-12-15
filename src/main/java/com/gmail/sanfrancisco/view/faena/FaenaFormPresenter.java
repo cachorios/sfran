ca473910b99d27b0b58b3a24de4b36cd7b0b188f
cachorios <cachorios@gmail.com>
2018-12-15 18:52:20 -0300

    faena

--- src/main/java/com/gmail/sanfrancisco/view/faena/FaenaFormPresenter.java
+++ src/main/java/com/gmail/sanfrancisco/view/faena/FaenaFormPresenter.java
@@ -1,14 +1,27 @@
 package com.gmail.sanfrancisco.view.faena;
 
 import com.gmail.cacho.slapi.view.controllers.AbstractPresenterForm;
+import com.gmail.cacho.slbase.core.Fecha;
+import com.gmail.cacho.slbase.persist.excepciones.PersistErrorException;
 import com.gmail.sanfrancisco.entidad.Faena;
 import com.gmail.sanfrancisco.serviciosModelo.FaenaService;
 
 import javax.inject.Inject;
+import java.util.Date;
 
 public class FaenaFormPresenter extends AbstractPresenterForm<Faena, FaenaService> {
     @Inject
     public FaenaFormPresenter(FaenaService servicio) {
         super(servicio);
+
+        Faena faena = new Faena();
+        faena.setFecha(new Date());
+        faena.setNumero(12);
+        try {
+            servicio.save(faena);
+        }catch (PersistErrorException e){
+            e.printStackTrace();
+        }
+
     }
 }
